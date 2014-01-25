4abb7a722c6fe2950dd9f3949a4c10a42ac436ee
Scott Mobley <Zott820@yahoo.com>
2014-01-25 14:45:16 -0800

    Submitted some parse files and tongue collider

---
+++ AgentSmith/Assets/Scripts/TongueCollide.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TongueCollide : MonoBehaviour {
+
+	public GameObject whipOwner;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider datCollision){
+		if(datCollision.collider.tag == "Player"){
+			whipOwner = this.transform.root.gameObject;
+		}
+	}
+
+}
