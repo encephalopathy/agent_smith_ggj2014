c1c25239a792fe42c50245878ea0069bda6750e1
Kellie Medlin <kelliem@fb.com>
2014-01-25 04:37:01 -0800

    Finish basic login UI visual flow

--- AgentSmith/Assets/Scripts/LoginGui.cs
+++ AgentSmith/Assets/Scripts/LoginGui.cs
@@ -1,20 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-using Facebook;
-
 public class LoginGui : MonoBehaviour {
 
-	enum GUIState { Login, Success, Failure, Instruction, Countdown }
-	GUIState currentState = GUIState.Login;
+	public enum GUIState { Login, Success, Failure, Instruction, Countdown, Finish }
+	public GUIState currentState = GUIState.Login;
 	
-	Texture2D leftBracket = null;
-	Texture2D rightBracket = null;
+	public Texture2D leftBracket = null;
+	public Texture2D rightBracket = null;
+	public Texture2D testTexture = null;
 	
 	void OnGUI () {
-		int width = Screen.width / 4;
-		int height = Screen.height;
-		
+		GUI.skin.label.alignment = TextAnchor.MiddleCenter;
+
 		switch (currentState) {
 			case GUIState.Login:
 				LoginScreen();
@@ -39,6 +37,11 @@ public class LoginGui : MonoBehaviour {
 		return new Rect(centerX - width / 2, centerY - height / 2, width, height);
 	}
 
+	void surroundWithBrackets (Rect rect) {
+		GUI.Label ( centerOn (rect.x, rect.y + 25, 20, 50), leftBracket);
+		GUI.Label ( centerOn (rect.x + rect.width, rect.y + 25, 20, 50), rightBracket);
+	}
+
 	void SetInit () {
 		System.Console.WriteLine("set init");
 	}
@@ -47,10 +50,7 @@ public class LoginGui : MonoBehaviour {
 		System.Console.WriteLine("on hide");
 	}
 
-	void LoginScreen () {
-		float width = Screen.width * 0.25f;
-		float height= Screen.height;
-		
+	void LoginScreen () {		
 		float groupWidth = Screen.width * 0.5f;
 		float groupHeight = Screen.height * 0.4f;
 		GUI.BeginGroup(centerOn(Screen.width * 0.5f, Screen.height * 0.75f, groupWidth, groupHeight));
@@ -59,21 +59,51 @@ public class LoginGui : MonoBehaviour {
 		
 		float textWidth = groupWidth * 0.5f;
 		float textHeight = 25;
-		GUI.Label ( centerOn(groupWidth * 0.5f, groupHeight * 0.33f, textWidth, textHeight * 2), "# agents ready\nwaiting for # agents");
-		GUI.Label (new Rect(0, 0, 100, 100), leftBracket);
+
+		Rect textDimensions = centerOn(groupWidth * 0.5f, groupHeight * 0.33f, textWidth, textHeight * 2);
+		GUI.Label ( textDimensions, "# agents ready\nwaiting for # agents");
+
+		// Decorate the center text with some fancy brackets.
+		surroundWithBrackets(textDimensions);
 		
 		if (GUI.Button (centerOn(groupWidth * 0.5f, groupHeight * 0.66f, textWidth, textHeight), "Login")) {
 			currentState = GUIState.Success;
-			FB.Init(SetInit, OnHideUnity, null);
+			//FB.Init(SetInit, OnHideUnity, null);
 		}
 		
 		GUI.EndGroup();
 	}
 	
 	void LoginSuccessScreen () {
-		if (GUI.Button (new Rect(20,40,80,20), "SUCCESS!")) {
-			currentState = GUIState.Success;
+		float groupWidth = Screen.width * 0.5f;
+		float groupHeight = Screen.height * 1f;
+		GUI.BeginGroup(centerOn(Screen.width * 0.5f, Screen.height * 0.5f, groupWidth, groupHeight));
+		
+		GUI.Box (new Rect(0, 0, groupWidth, groupHeight), "");
+		
+		float textWidth = groupWidth * 0.5f;
+		float textHeight = 25;
+		
+		Rect textDimensions = centerOn(groupWidth * 0.5f, groupHeight * 0.15f, textWidth, textHeight * 2);
+		GUI.Label ( textDimensions, "WELCOME\nAgent Foo Bar Baz");
+		
+		// Decorate the center text with some fancy brackets.
+		surroundWithBrackets(textDimensions);
+
+		// Player texture!
+		float playerHeight = groupHeight - textDimensions.y - groupHeight * 0.3f;
+		GUI.Label ( centerOn (groupWidth * 0.5f, textDimensions.y + playerHeight * 0.5f, testTexture.width, testTexture.height), testTexture);
+
+		GUI.Label ( centerOn (groupWidth * 0.5f, groupHeight * 0.7f, textWidth, textHeight * 3.0f),
+		           "We have given you the CIRCLE persona.\n" +
+		           "Your mission is to turn those who are not you into how you see yourself.");
+
+		if (GUI.Button (centerOn(groupWidth * 0.5f, groupHeight * 0.85f, textWidth, textHeight), "Continue")) {
+			currentState = GUIState.Instruction;
+			//FB.Init(SetInit, OnHideUnity, null);
 		}
+		
+		GUI.EndGroup();
 	}
 	
 	void LoginFailureScreen () {
@@ -81,10 +111,54 @@ public class LoginGui : MonoBehaviour {
 	}
 	
 	void InstructionScreen () {
+		float groupWidth = Screen.width * 0.5f;
+		float groupHeight = Screen.height * 1f;
+		GUI.BeginGroup(centerOn(Screen.width * 0.5f, Screen.height * 0.5f, groupWidth, groupHeight));
 		
+		GUI.Box (new Rect(0, 0, groupWidth, groupHeight), "");
+		
+		float textWidth = groupWidth * 0.5f;
+		float textHeight = 25;
+
+		GUI.Label ( centerOn (groupWidth * 0.2f, groupHeight * 0.15f, groupWidth * 0.2f, groupHeight * 0.3f), "Your enemies are those\nwho appear different");
+		GUI.Label ( centerOn (groupWidth * 0.2f, groupHeight * 0.4f, groupWidth * 0.2f, groupHeight * 0.3f), "Move with WASD\nAttack with SPACE");
+		GUI.Label ( centerOn (groupWidth * 0.2f, groupHeight * 0.65f, groupWidth * 0.4f, groupHeight * 0.3f), "Hit them, and assimilate\nyour foes to your side");
+
+		GUI.Label ( centerOn (groupWidth * 0.65f, groupHeight * 0.15f, groupWidth * 0.7f, groupHeight * 0.3f), testTexture);
+		GUI.Label ( centerOn (groupWidth * 0.65f, groupHeight * 0.4f, groupWidth * 0.7f, groupHeight * 0.3f), testTexture);
+		GUI.Label ( centerOn (groupWidth * 0.65f, groupHeight * 0.65f, groupWidth * 0.7f, groupHeight * 0.3f), testTexture);
+
+		GUI.Label ( centerOn (groupWidth * 0.5f, groupHeight * 0.28f, groupWidth * 1f, 20), "_____________________________________________________________________________");
+		GUI.Label ( centerOn (groupWidth * 0.5f, groupHeight * 0.53f, groupWidth * 1f, 20), "_____________________________________________________________________________");
+	
+		if (GUI.Button (centerOn(groupWidth * 0.5f, groupHeight * 0.85f, textWidth, textHeight), "Continue")) {
+			currentState = GUIState.Countdown;
+		}
+		
+		GUI.EndGroup();	
 	}
 	
 	void CountdownScreen () {
+		float groupWidth = Screen.width * 0.5f;
+		float groupHeight = Screen.height * 0.25f;
+		GUI.BeginGroup(centerOn(Screen.width * 0.5f, Screen.height * 0.5f, groupWidth, groupHeight));
+		GUI.Box (new Rect(0, 0, groupWidth, groupHeight), "");
+
+		Rect textDimensions = centerOn (groupWidth * 0.5f, groupHeight * 0.5f, groupWidth * 0.5f, 50);
+		GUI.Label ( textDimensions, "Waiting on # agents...");
+		surroundWithBrackets(textDimensions);
+		GUI.EndGroup();
+	}
+
+	void FinishScreen () {
+		float groupWidth = Screen.width * 0.5f;
+		float groupHeight = Screen.height * 0.25f;
+		GUI.BeginGroup(centerOn(Screen.width * 0.5f, Screen.height * 0.5f, groupWidth, groupHeight));
+		GUI.Box (new Rect(0, 0, groupWidth, groupHeight), "");
 		
+		Rect textDimensions = centerOn (groupWidth * 0.5f, groupHeight * 0.5f, groupWidth * 0.5f, 50);
+		GUI.Label ( textDimensions, "Entering in # seconds\nGood luck, Agent Foo");
+		surroundWithBrackets(textDimensions);
+		GUI.EndGroup();
 	}
 }
