a3bc88f634f14d65b7e9532c49126e088adf14f6
Kellie Medlin <kelliem@fb.com>
2014-01-25 13:11:03 -0800

    Created GameStart script

---
+++ agentsmith/Assets/Scripts/GameStart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStart : MonoBehaviour {
+
+	public Object PlayerPrefab = null;
+
+	// Use this for initialization
+	void Start () {
+		Network.Instantiate(PlayerPrefab, Vector3.up * 5, Quaternion.identity, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
