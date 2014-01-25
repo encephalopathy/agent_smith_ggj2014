c962aaeb00740bb4e467dc87b20996dfb8b20b46
Scott Mobley <Zott820@yahoo.com>
2014-01-25 10:39:41 -0800

    Added a rudimentary Whip - Needs to be attached to the Player Working on script to animate the whip

---
+++ AgentSmith/Assets/tongueAnimate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tongueAnimate : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKey(KeyCode.Space)) {
+			transform.localScale = Vector3.Lerp (Vector3.one, Vector3.one * 3.0f, Time.deltaTime);
+		}
+	
+	}
+}
